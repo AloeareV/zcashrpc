b3f8f8a0a13dea76956a1609e2aeabac82589d80
zancas <zancas@zingolabs.com>
2020-09-28 10:15:51 -0600

    reformat macro

--- tests/zcashrpc-smoke-tests.rs
+++ tests/zcashrpc-smoke-tests.rs
@@ -2,7 +2,10 @@ macro_rules! run_smoketest {
     ($x:ident) => {
         #[tokio::test]
         async fn $x() {
-            let _res = zcashrpc::client::utils::make_client(true).$x().await.unwrap();
+            let _res = zcashrpc::client::utils::make_client(true)
+                .$x()
+                .await
+                .unwrap();
         }
     };
 }
